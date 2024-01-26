a8aaa1d6bdb5d8b4c5d87529a706e94f5c844321
MisbahQureshi <misbah.rizwan.qureshi@gmail.com>
2024-01-26 23:05:53 +0500

    Initial Commit

---
+++ src/Heading.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Heading() {
+  return (
+    <header className='header'>
+        <img className='headerImg' src="./images/troll.png" alt="" />
+        <h2 className='headerTitle'>Meme Generator</h2>
+        <h4 className='headerProject'>By - Misbah Qureshi</h4>
+        </header>
+  )
+}
+
+export default Heading
